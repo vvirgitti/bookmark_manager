3b42e818206ca055563f92f8b4ab8a40b09a68fc
Vanessa <virgitti@gmail.com>
2015-02-24 16:07:28 +0000

    Created link.rb and spec and spec helper

--- app/server.rb
+++ app/server.rb
@@ -1,8 +1,18 @@
 require 'sinatra/base'
+require 'data_mapper'
 
 class Bookmark < Sinatra::Base
 
+  env = ENV['RACK_ENV'] || 'development'
+
   set :views, Proc.new {File.join(root, "..", "views")}
+
+  DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+  require './lib/link'
+
+  DataMapper.finalize
+  DataMapper.auto_upgrade!
 
   get '/' do
     'Hello Bookmark!'
---
+++ app/spec/link_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe Link do
+s
+
+end
