14861b5516e64c6b009bdbe37112810dd4e8ecf9
Vanessa <virgitti@gmail.com>
2015-02-25 16:35:02 +0000

    session created

--- app/server.rb
+++ app/server.rb
@@ -5,12 +5,17 @@ class BookmarkManager < Sinatra::Base
 
   env = ENV['RACK_ENV'] || 'development'
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   set :views, Proc.new {File.join(root, "..", "views")}
 
   DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
   require_relative './lib/link'
   require_relative './lib/tag'
+  require_relative './lib/user'
+
 
   DataMapper.finalize
   DataMapper.auto_upgrade!
@@ -35,6 +40,23 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first_or_create(text: params[:tag])
     @links = tag ? tag.links : []
     erb :index
+  end
+
+  get '/users/new' do
+    erb :"users/new"
+  end
+
+  post '/users' do
+    user = User.create(:email => params[:email],
+                :password => params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
   end
 
 
